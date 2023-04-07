22ed8d3c69f7265a8b03bc642b298a099c207aad
Leon Parsons <MY_NAME@example.com>
2023-04-07 15:12:24 +1200

    Adds random name generation functionality

--- src/components/CharName.js
+++ src/components/CharName.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 
 const CharName = () => {
+
+  const [chosenName, setChosenName] = useState("");
+
+  const firstName = ["Jorr", "Keela", "Jeroy", "Rishka", "Leer", "Kall", "Jenra", "Yan", "Kurk"];
+  const lastName = ["Rorrison", "Gorrax", "Karlo", "Torak", "Raddok", "Yorra"];
+
+  const randomiseName = () => {
+    const randomNum1 = Math.floor(Math.random() * firstName.length);
+    const randomNum2 = Math.floor(Math.random() * lastName.length);
+    setChosenName(firstName[randomNum1] + " " + lastName[randomNum2]); 
+    setChosen
+  }
+
+
   return (
     <div className="name">
       <h4>Enter Name</h4>
-      <input></input>
+      <input value={chosenName}></input>
+      <button onClick={randomiseName}>Randomise</button>
     </div>
   )
 }
